5b7ad449b67906530b63c835cb1de8df31937141
AnaTeresaCastro <ana.1148@hotmail.com>
2024-09-12 16:31:05 +0100

    Added javascript single location clock info

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,20 @@
+function showTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDate = losAngelesElement.querySelector(".date");
+  let losAngelesTime = losAngelesElement.querySelector(".time");
+  losAngelesDate.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("MMMM Do YYYY");
+  losAngelesTime.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
 
+  let ParisElement = document.querySelector("#paris");
+  let ParisDate = ParisElement.querySelector(".date");
+  let ParisTime = ParisElement.querySelector(".time");
+  ParisDate.innerHTML = moment().tz("Europe/Paris").format("MMMM Do YYYY");
+  ParisTime.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+setInterval(showTime, 1000);
