1bcb7a80684a2cb0d6208076d3fc3d7e3567c3d4
AnaTeresaCastro <ana.1148@hotmail.com>
2024-09-13 12:03:11 +0100

    Adde javascript multiple locations

--- javascript/index.js
+++ javascript/index.js
@@ -18,3 +18,19 @@ function showTime() {
     .format("h:mm:ss [<small>]A[</small>]");
 }
 setInterval(showTime, 1000);
+
+function showTimeCity(event) {
+  let citySearch = document.querySelector("#cities");
+  let cityName = event.target.value.replace("_", " ").split("/")[1];
+  let cityTz = moment().tz(event.target.value);
+  citySearch.innerHTML = `<h2>${cityName}</h2>
+        <div class="city" id="los-angeles">
+          <div class="date">${cityTz.format("MMMM Do YYYY")}</div>
+          <div class="time">${cityTz.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>`;
+}
+
+let selectElement = document.querySelector("#city");
+selectElement.addEventListener("change", showTimeCity);
